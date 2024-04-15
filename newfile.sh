a33093d92160472d2b39f61b57e2d75206da4a4f
astine85 <astinebeess56@gmail.com>
2024-04-15 21:52:33 +0100

    updated dupsys file

--- newfile.sh
+++ newfile.sh
@@ -1,3 +1,6 @@
 This is a test file to practice my git skills
 Weldone dupsy. It should work now.
-Hello
+Hello, so i was saying that we thank Dr Ilesanmi oooo.
+This course is not easy. 17th April is here already to collect money again.
+Weldone Sir, Dupe is grateful oooo.
+
